0034d00995069f1de8f0dca15de27f765cfb962f
SuperChed <springac85@gmail.com>
2022-02-27 00:16:45 -0500

    comments for alternative code

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,12 +1,19 @@
 <template>
   <p v-if="num < 300">{{ num }}ms, much skill!</p>
   <p v-if="num > 300 && num < 500">{{ num }}ms, practice more!</p>
-  <p v-if="num > 700">{{ num }}ms, don't hurt yourself!</p>
+  <p v-if="num > 700">{{ num }}ms, so slowness!</p>
 </template>
 
 <script>
 export default {
     props: [ 'num' ],
+
+// could also put logic for rank/score here
+// in mounted() using if statements to update
+// a new variable, then displaying that in
+// another p or span...
+// data handled in template expression comes
+// in just before mounted(), or after ???
 
 }
 </script>
