505319b265d3970370fbf238503306f71268d5e6
SuperChed <springac85@gmail.com>
2022-02-27 00:04:56 -0500

    updated to include results component

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,9 +1,12 @@
 <template>
-  
+  <p v-if="num < 300">{{ num }}ms, much skill!</p>
+  <p v-if="num > 300 && num < 500">{{ num }}ms, practice more!</p>
+  <p v-if="num > 700">{{ num }}ms, don't hurt yourself!</p>
 </template>
 
 <script>
 export default {
+    props: [ 'num' ],
 
 }
 </script>
